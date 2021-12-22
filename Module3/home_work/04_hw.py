f0d09f604b88477b349f8d33f2f838ed432dd0d9
IgorTresha <96468114+IgorTresha@users.noreply.github.com>
2021-12-22 22:39:08 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,18 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+
+list_exampl = []
+n = int(input("Введите n= "))
+cnt = 0
+while cnt < n:
+    list_exampl.append(random.randint(-100, 100))
+    cnt += 1
+print("Дано: ", list_exampl)
+new_list = []
+for var in list_exampl:
+    if var > 0 and var ** 0.5 % 1 == 0:
+        new_list.append(round(var ** 0.5))
+print("Answer: ", new_list)
