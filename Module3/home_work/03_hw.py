a18ad2ea89ce5004c2b308cf5286db830ca42ecf
IgorTresha <96468114+IgorTresha@users.noreply.github.com>
2021-12-22 22:38:05 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,19 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+
+import random
+
+numbers = []
+n = int(input("Введите n= "))
+cnt = 0
+while cnt < n:
+    numbers.append(random.randint(-100, 100))
+    cnt += 1
+print(numbers)
+
+total_summ = 0
+for number in numbers:
+    if number > 0 and number % 2 == 0:
+        total_summ = total_summ + number
+print("Сумма положительных элементов кратных 2 =", total_summ)
